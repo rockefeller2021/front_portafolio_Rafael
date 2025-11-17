1dd357e589bdc62f9bc2667c1581f1aa8ccebb55
rafael alvarado <magomlg@gmail.com>
2025-11-17 10:27:57 -0500

    Configuración inicial de Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,5 @@ export const environment = {
   // OJO: Esta URL es un EJEMPLO.
   // La URL real la obtendrás cuando despliegues tu backend en Google Cloud Run.
   // Tendrás que venir a este archivo y reemplazarla por la URL que te dé Google.
-  apiUrl: 'https://portfolio-api-xxxxxxxx-uc.a.run.app/api/v1'
+  apiUrl: 'https://portfolio-api-554626914949.us-central1.run.app/api/v1'
 };
